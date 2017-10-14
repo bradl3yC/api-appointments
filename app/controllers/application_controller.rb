f7f0aa588a8c2feda1f49c1aff237c9f2cc1a493
Brad Conley <brad.conl3y@gmail.com>
2017-10-14 17:56:12 -0400

    Change port

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::API
   include ActionController::Serialization
   acts_as_token_authentication_handler_for User, fallback: :none
-  Rails.configuration.action_controller.per_form_csrf_tokens = false
 end
