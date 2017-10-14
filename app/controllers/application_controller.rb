c41973ffc4ffb36a46dcb44e6ed5c19a99ba2099
Brad Conley <brad.conl3y@gmail.com>
2017-10-14 17:30:32 -0400

    lazy commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
   include ActionController::Serialization
-  skip_before_action :verify_authenticity_token
   acts_as_token_authentication_handler_for User, fallback: :none
+  Rails.configuration.action_controller.per_form_csrf_tokens = false
 end
